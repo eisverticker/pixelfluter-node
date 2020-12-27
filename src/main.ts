a18f45a06b5d82de5240d5f0a738cff5c0f3b790
lalex <eisverticker@posteo.de>
2020-12-27 16:13:34 +0100

    refactor: encapsulate code inside classes

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,85 @@ import Net from 'net'
 const port = 1337
 const host = 'localhost'
 
-const client = new Net.Socket()
-client.connect(
-  { port: port, host: host }, function() {
+const Color = {
+  RED: 'ff0000',
+  GREEN: '00ff00',
+  WHITE: 'ffffff'
+}
 
-    console.log('TCP connection established with the server.')
-    
-    for(let i = 0; i < 2000; i++) {
-      for(let x = 0; x < 200; x++) {
-        for (let y = 0; y < 200; y++) {
-          client.write(`PX ${x} ${y} ffffff\n`)
+function randomInt(min: number, max: number) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+
+
+class Pixel {
+  constructor (private x: number, private y: number, private color?: string) {}
+
+  getX() {
+    return this.x
+  }
+
+  getY() {
+    return this.y
+  }
+
+  getColor(): string | undefined {
+    return this.color
+  }
+
+  toCommand (): string {
+    return `PX ${this.x} ${this.y} ${this.color}`
+  }
+
+  static parseCommand(command: string): Pixel {
+    const parts = command.split(' ')
+
+    if (parts.length < 3) {
+      throw new Error('failed to parse command ' + command + 'due to wrong format')
+    }
+
+    return new Pixel(parseInt(parts[1]), parseInt(parts[2]), parts[3])
+  }
+
+}
+
+class ScreenDrawer {
+  public client: Net.Socket
+
+  constructor(private port: number, private host: string) {
+    this.client = new Net.Socket()
+  }
+
+  drawRectangle(xOffset: number, yOffset: number, width: number, height: number, color = Color.RED) {
+    const rounds = 200
+
+    for(let i = 0; i < rounds; i++) {
+      for(let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+          const pixel = new Pixel(x + xOffset, y + yOffset, color)
+          this.client.write(pixel.toCommand())
         }
       }
     }
-  })
+  }
 
-client.on('data', function(chunk) {
-  console.log(`Data received from the server: ${chunk.toString()}.`)
-  client.end()
-})
+  start (callback: any) {
+    this.client.connect({ port: this.port, host: this.host }, callback)
+    this.client.on('data', function(chunk) {
+      // console.log(`Incoming: ${chunk.toString()}.`)
+      client.end()
+    })
+    
+    this.client.on('end', function() {
+      console.log('Requested an end to the TCP connection')
+    })
+  }
+}
 
-client.on('end', function() {
-  console.log('Requested an end to the TCP connection')
+const client = new Net.Socket()
+const screenDrawer = new ScreenDrawer(port, host)
+screenDrawer.start(() => {
+  screenDrawer.drawRectangle(randomInt(0, 1920 - 200), randomInt(0, 1080 - 200), 200, 200)
 })
